6e8db4748348f95f346b0eed086fe54b872b4b46
George <zloi.tatarin@yahoo.com>
2015-03-26 16:43:18 -0700

    Set up first API endpoints.

--- walktalk/urls.py
+++ walktalk/urls.py
@@ -2,9 +2,7 @@ from django.conf.urls import patterns, include, url
 from django.contrib import admin
 
 urlpatterns = patterns('',
-    # Examples:
-    # url(r'^$', 'walktalk.views.home', name='home'),
-    # url(r'^blog/', include('blog.urls')),
-
-    url(r'^admin/', include(admin.site.urls)),
+    url(r'^login/', 'backend.views.login', name='login'),
+    url(r'^register/', 'backend.views.register', name='register'),
+    # url(r'^admin/', include(admin.site.urls)),
 )
