4b8d8cb4da202ad1fd895cd9afbfe93127a26cf4
George <zloi.tatarin@yahoo.com>
2015-03-26 16:49:21 -0700

    Clean up tests a bit.

--- backend/tests.py
+++ backend/tests.py
@@ -8,7 +8,6 @@ class LoginTestCase(TestCase):
     def setUp(self):
         # http://www.xorbin.com/tools/sha256-hash-calculator
         # Hash of "testpassword"
-
         User.objects.create(
             username="gk",
             password="9f735e0df9a1ddc702bf0a1a7b83033f9f7153a00c29de82cedadc9957289b05",
@@ -26,19 +25,14 @@ class LoginTestCase(TestCase):
         self.assertTrue(login.is_valid())
         self.assertTrue("user_object" in login.cleaned_data)
         self.assertTrue(isinstance(login.cleaned_data["user_object"], User))
+        self.assertTrue(login.cleaned_data["user_object"].sex in [
+            x[0] for x in User.SEX_CHOICES])
+        print(login.cleaned_data["user_object"].asJSON())
 
     def test_login_badpassword(self):
         login = LoginForm({
             "username": "gk",
             "password": "bad password"
-        })
-
-        self.assertEqual(login.is_valid(), False)
-        self.assertTrue(errors.get_error("login") in login.errors.get("__all__"))
-
-        login = LoginForm({
-            "username": "bad username",
-            "password": "9f735e0df9a1ddc702bf0a1a7b83033f9f7153a00c29de82cedadc9957289b05"
         })
 
         self.assertEqual(login.is_valid(), False)
