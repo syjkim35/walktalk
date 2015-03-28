d197855a3c70c1348d4f167ac5012809c3c63277
George <zloi.tatarin@yahoo.com>
2015-03-27 22:36:52 -0700

    Move decorators to a more appropriate place.

--- walktalk/utils.py
+++ walktalk/utils.py
@@ -1,5 +1,8 @@
 import json
 
+from django.http import HttpResponse
+from walktalk    import errors
+
 ZEROES = '0' * ((24 * 60) // 30)     # time chunks
 WEEKDAYS = [
     "monday", "tuesday", "wednesday", "thursday",
@@ -16,3 +19,33 @@ def is_int(value):
 
     except ValueError:
         return False
+
+def only_post(fn):
+    def wrapper(request, *args):
+        if request.method != "POST":
+            return HttpResponse("Bad request: " + str(request.method),
+                                status=405)
+
+        return fn(request, *args)
+
+    return wrapper
+
+def only_get(fn):
+    def wrapper(request, *args):
+        if request.method != "GET":
+            return HttpResponse("Bad request: " + str(request.method),
+                                status=405)
+
+        return fn(request, *args)
+
+    return wrapper
+
+def authorized(fn):
+    def wrapper(request, *args):
+        if "authorized" not in request.session:
+            return HttpResponse(jsonify(
+                errors.make_error(errors.get_error("auth"), 403, None)))
+
+        return fn(request, *args)
+
+    return wrapper
