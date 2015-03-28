77e5b60440179372a427a5b08221d1ea23db3bbe
George <zloi.tatarin@yahoo.com>
2015-03-27 22:06:47 -0700

    Add default redirection.

--- walktalk/urls.py
+++ walktalk/urls.py
@@ -2,7 +2,9 @@ from django.conf.urls import patterns, include, url
 from django.contrib import admin
 
 urlpatterns = patterns('',
+    url(r'^$', 'backend.views.home'),
     url(r'^login/', 'backend.views.login', name='login'),
     url(r'^register/', 'backend.views.register', name='register'),
+    url(r'^schedule/', 'backend.views.schedule', name='schedule'),
     # url(r'^admin/', include(admin.site.urls)),
 )
