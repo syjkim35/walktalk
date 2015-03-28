968fe734f74eb75723c3334ef8472de51547a76b
George <zloi.tatarin@yahoo.com>
2015-03-27 22:08:07 -0700

    Add auth errors.

--- walktalk/errors.py
+++ walktalk/errors.py
@@ -1,4 +1,5 @@
 ERRORS = {
+    "auth":                         "User not authorized.",
     "login":                        "The username/password that you entered is invalid.",
     "register_username_exists":     "This username already exists.",
     "register_password_mismatch":   "The passwords do not match.",
@@ -8,18 +9,15 @@ def get_error(key):
     return ERRORS.get(key, "An error occurred")
 
 def convert_errors(django_errors, error_code=404):
-    custom_error = {}
-    custom_error["code"] = error_code
-
     if "__all__" in django_errors:
-        custom_error["field"] = None
-        custom_error["error"] = django_errors["__all__"][0]
+        field = None
+        error = django_errors["__all__"][0]
     else:
         first_error = django_errors.keys()[0]
-        custom_error["field"] = first_error
-        custom_error["error"] = django_errors[first_error]
+        field = first_error
+        error = django_errors[first_error]
 
-    return custom_error
+    return make_error(error, error_code, field)
 
 def make_error(error_str, error_code, field):
     return {
