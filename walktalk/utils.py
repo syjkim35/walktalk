580c2e528ec158b00e6dd1aded39ed73d9f87320
George <zloi.tatarin@yahoo.com>
2015-03-27 22:09:46 -0700

    Add schedule data utilities.

--- walktalk/utils.py
+++ walktalk/utils.py
@@ -1,4 +1,18 @@
 import json
 
+ZEROES = '0' * ((24 * 60) // 30)     # time chunks
+WEEKDAYS = [
+    "monday", "tuesday", "wednesday", "thursday",
+    "friday", "saturday", "sunday"
+]
+
 def jsonify(data):
     return json.dumps(data)
+
+def is_int(value):
+    try:
+        int(value)
+        return True
+
+    except ValueError:
+        return False
